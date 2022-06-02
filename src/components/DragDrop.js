33cbb2763091c3f38ff36c5d8dd2b3c0b5e448b8
Uzair Aslam <uaslam@mjhlifesciences.com>
2022-06-02 16:29:27 -0500

    added drag drop component

--- src/components/DragDrop.js
+++ src/components/DragDrop.js
@@ -4,46 +4,119 @@ The shape should change color as it moves from the center to the edge of the scr
 -Allow user to choose the type of shape and a sequence of changing colors from a dropdown list.
 
 */
-import React, {useState, useEffect} from 'react'
+import React, {useState, useEffect, useRef} from 'react'
+import Select from 'react-select';
 
 const DragDropComponent = () => {
+
+    const shapeRef = useRef()
+
+    const shapeMovementDetails = useRef({active: false, currentX: 0, currentY : 0, initialX: 0, initialY: 0, xOffset: 0, yOffset: 0})
 
     const [selectedShape, setSelectedShape] = useState(null)
     // would be a classname 
     const [styling, setStyling] = useState('')
+    const [selectedColors, setSelectedColors] = useState([])
 
-    const handleSelect = (e) => {
-        console.log(e.target.value)
-        setSelectedShape(e.target.value)
+   useEffect(() => {
+    let shape = shapeRef.current
+    console.log('how often does this useeffect run')
+
+    const enableDrag = (e) => {
+      console.log('i m mousedown event: ', e)
+      shapeMovementDetails.current.initialX = e.clientX - shapeMovementDetails.current.xOffset
+      shapeMovementDetails.current.initialY = e.clientY - shapeMovementDetails.current.yOffset
+      if (e.target === shape){
+        shapeMovementDetails.current.active = true
+      }
+  }
+
+  const disableDrag = (e) => {
+    console.log('i m mouseup event: ', e)
+    shapeMovementDetails.current.initialX =  shapeMovementDetails.current.currentX
+    shapeMovementDetails.current.initialY =  shapeMovementDetails.current.currentY
+    shapeMovementDetails.current.active = false
     }
-    console.log('i am setselected shape', selectedShape)
 
-   
+  let generateRandomColorAtAveryMove = () => {
+
+      let colorChoice = selectedColors.map(colorObj => colorObj.value)
+
+      return colorChoice[Math.floor(Math.random()*colorChoice.length)]
+      
+  }
+
+
+const drag = (e) => {
+  // console.log('i m drag event: ', e)
+  if ( shapeMovementDetails.current.active) {
+  e.preventDefault()
+  
+  shapeMovementDetails.current.currentX = e.clientX - shapeMovementDetails.current.initialX
+  shapeMovementDetails.current.currentY = e.clientY - shapeMovementDetails.current.initialY
+
+  shapeMovementDetails.current.xOffset = shapeMovementDetails.current.currentX
+  shapeMovementDetails.current.yOffset = shapeMovementDetails.current.currentY
+
+  shape.style.transform = "translate3d(" + shapeMovementDetails.current.currentX + "px, " + shapeMovementDetails.current.currentY + "px, 0)"
+  shape.style.backgroundColor =  generateRandomColorAtAveryMove()
+
+  }
+}
+
+
+      shape.addEventListener("mousedown", enableDrag, false)
+      shape.addEventListener("mouseup", disableDrag, false)
+      shape.addEventListener("mousemove", drag, false)
+
+    return () => {
+      shape.removeEventListener("mousedown", enableDrag)
+      shape.removeEventListener("mouseup", disableDrag)
+      shape.removeEventListener("mousemove", drag)
+    }
+
+   },[selectedColors])
+
+
 
 useEffect(() => {
-
-    console.log('do i run everytime')
     if(selectedShape === 'circle'){
         setStyling('circle')
-    }else if(selectedShape === 'triangle'){
-        setStyling('triangle')
+    }else if(selectedShape === 'rectangle'){
+        setStyling('rectangle')
     }
 
 }, [selectedShape])
 
+const shapeOptions = [
+    { value: 'circle', label: 'circle' },
+    { value: 'rectangle', label: 'rectangle' },
+  ];
+
+const colorOptions = [
+  {value: 'red', label: 'red'},
+  {value: 'green', label: 'green'},
+  {value: 'orange', label: 'orange'},
+  {value: 'blue', label: 'blue'}
+]
+
 
     return(<>
-        <form>
-  <label for="cars">Choose a shape:</label>
-   <select onChange={handleSelect} name="shape_selector" id="cars">
-    <option value=""></option>
-    <option value="circle">Circle</option>
-    <option value="triangle">Triangle</option> 
-  </select>
-  <br /><br />
-  <input type="submit" value="Submit" />
-        </form>
-        <div className={styling}></div>
+    <div>   
+   <h4>Select shape </h4>
+   <Select
+          onChange={(e) => setSelectedShape(e.label)}
+          options={shapeOptions}
+        />
+   <h4>Select colors (could be more than one) </h4>
+    <Select
+          isMulti={true}
+          onChange={(e) => setSelectedColors(e)}
+          options={colorOptions}
+        />    
+  
+  </div>
+ <div ref={shapeRef} className={styling}></div>
       </>
   )
 
